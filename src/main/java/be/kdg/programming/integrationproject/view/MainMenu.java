624a1d2ba0e8b28207745e83cee7232745c0392b
J4spr <jasper07.verbruggen@gmail.com>
2026-03-05 14:09:23 +0100

    update pom.xml and module info

---
+++ src/main/java/be/kdg/programming/integrationproject/view/MainMenu.java
@@ -0,0 +1,7 @@
+package be.kdg.programming.integrationproject.view;
+
+import javafx.scene.layout.BorderPane;
+
+public class MainView extends BorderPane {
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,5 @@ module be.kdg.programming.integrationproject {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens be.kdg.programming.integrationproject to javafx.fxml;
     exports be.kdg.programming.integrationproject;
 }
