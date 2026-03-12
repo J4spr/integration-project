e675da09e8094c3de6e3ab6cf82b61acf5b51735
YariDeSchepper <yari.deschepper@student.kdg.be>
2026-03-12 02:19:52 +0100

    Add QuiltBoard base structure with grid and placedPatches list

--- src/main/java/be/kdg/programming/integrationproject/model/QuiltBoard.java
+++ src/main/java/be/kdg/programming/integrationproject/model/QuiltBoard.java
@@ -1,4 +1,10 @@
 package be.kdg.programming.integrationproject.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuiltBoard {
+    private static final int SIZE = 9;
+    private final boolean[][] grid = new boolean[SIZE][SIZE];
+    private final List<Patch> placedPatches = new ArrayList<>();
 }
