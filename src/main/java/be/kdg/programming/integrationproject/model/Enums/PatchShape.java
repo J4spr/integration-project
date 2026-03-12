662e0b9b4bdf3cb5481f3d3e48601090d4c92bc4
YariDeSchepper <yari.deschepper@student.kdg.be>
2026-03-12 02:40:48 +0100

    Add PatchShape enum base structure with first shape

--- src/main/java/be/kdg/programming/integrationproject/model/Enums/PatchShape.java
+++ src/main/java/be/kdg/programming/integrationproject/model/Enums/PatchShape.java
@@ -1,4 +1,17 @@
 package be.kdg.programming.integrationproject.model.Enums;
 
 public enum PatchShape {
+    SMALL(new boolean[][]{
+            {true, true}
+    });
+
+    private final boolean[][] shape;
+
+    PatchShape(boolean[][] shape) {
+        this.shape = shape;
+    }
+
+    public boolean[][] getShape() {
+        return this.shape;
+    }
 }
