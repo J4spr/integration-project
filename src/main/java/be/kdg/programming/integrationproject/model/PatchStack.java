d2b807aedb8c25f5c6ebdd74f4387f3504226d92
YariDeSchepper <yari.deschepper@student.kdg.be>
2026-03-13 12:40:57 +0100

    Added methods removePatch and addPatch to model class PatchStack

--- src/main/java/be/kdg/programming/integrationproject/model/PatchStack.java
+++ src/main/java/be/kdg/programming/integrationproject/model/PatchStack.java
@@ -6,21 +6,39 @@ import java.util.List;
 
 public class PatchStack {
     private final LinkedList<Patch> patches = new LinkedList<>();
-    private int tokenPosition;
+    private int neutralTokenPosition;
 
     //Returns the 3 available patches to the right of the neutral token.
     public List<Patch> getAvailablePatches() {
         List<Patch> available = new ArrayList<>();
         for (int i = 1; i <= 3; i++) {
-            int index = (tokenPosition + i) % patches.size();
+            int index = (neutralTokenPosition + i) % patches.size();
             available.add(patches.get(index));
         }
         return available;
     }
 
-     //Returns the position of the neutral token.
+     //Returns the position of the neutral token
     public int getNeutralToken() {
-        return this.tokenPosition;
+        return this.neutralTokenPosition;
+    }
+
+    //Adds a patch to the list
+    public void addPatch(Patch patch) {
+        patches.add(patch);
+    }
+
+     //Removes the patch with the given patchID from the list
+     //Moves the neutral token to the position of the removed patch
+     //Returns the removed patch
+    public Patch removePatch(int patchID) {
+        for (int i = 0; i < patches.size(); i++) {
+            if (patches.get(i).getPatchID() == patchID) {
+                neutralTokenPosition = i;
+                return patches.remove(i);
+            }
+        }
+        return null;
     }
 }
 
