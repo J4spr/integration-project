eae4a0122112c3f586d2727f086bdf5e26576fb0
YariDeSchepper <yari.deschepper@student.kdg.be>
2026-03-13 12:34:24 +0100

    Added methods getAvailablePatches and getNeutralToken to model class PatchStack

--- src/main/java/be/kdg/programming/integrationproject/model/PatchStack.java
+++ src/main/java/be/kdg/programming/integrationproject/model/PatchStack.java
@@ -1,10 +1,26 @@
 package be.kdg.programming.integrationproject.model;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class PatchStack {
     private final LinkedList<Patch> patches = new LinkedList<>();
     private int tokenPosition;
-    
+
+    //Returns the 3 available patches to the right of the neutral token.
+    public List<Patch> getAvailablePatches() {
+        List<Patch> available = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            int index = (tokenPosition + i) % patches.size();
+            available.add(patches.get(index));
+        }
+        return available;
+    }
+
+     //Returns the position of the neutral token.
+    public int getNeutralToken() {
+        return this.tokenPosition;
+    }
 }
 
