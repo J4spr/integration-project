1b0c5a1ff59f045cee3a37f3c638d4150766415e
YariDeSchepper <yari.deschepper@student.kdg.be>
2026-03-13 11:42:03 +0100

    Added PatchStack base structure with patches list and tokenPosition

--- src/main/java/be/kdg/programming/integrationproject/model/PatchStack.java
+++ src/main/java/be/kdg/programming/integrationproject/model/PatchStack.java
@@ -1,4 +1,10 @@
 package be.kdg.programming.integrationproject.model;
 
+import java.util.LinkedList;
+
 public class PatchStack {
+    private final LinkedList<Patch> patches = new LinkedList<>();
+    private int tokenPosition;
+    
 }
+
