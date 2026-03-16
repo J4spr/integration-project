a9cab3a24070ee5ea5dd692889a21962a0bb3987
YariDeSchepper <yari.deschepper@student.kdg.be>
2026-03-16 22:54:08 +0100

    Implement CpuPlayer model class + decideTurn method with random moves

--- src/main/java/be/kdg/programming/integrationproject/model/CpuPlayer.java
+++ src/main/java/be/kdg/programming/integrationproject/model/CpuPlayer.java
@@ -1,6 +1,10 @@
 package be.kdg.programming.integrationproject.model;
 
 import be.kdg.programming.integrationproject.model.Enums.Difficulty;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import be.kdg.programming.integrationproject.model.Enums.PatchRotation;
 
 public class CpuPlayer extends Player {
     private Difficulty difficulty;
@@ -19,6 +23,23 @@ public class CpuPlayer extends Player {
     }
 
     public void decideTurn(Game game) {
-        // implementatie komt later
+        List<Patch> availablePatches = game.getPatchStack().getAvailablePatches();
+        Collections.shuffle(availablePatches);
+
+        Random random = new Random();
+        PatchRotation[] rotations = PatchRotation.values();
+
+        for (Patch patch : availablePatches) {
+            if (game.getCurrentPlayer().getTotalButtons() < patch.getButtonCost()) continue;
+
+            for (int attempt = 0; attempt < 20; attempt++) {
+                PatchRotation randomRotation = rotations[random.nextInt(rotations.length)];
+                int randomRow = random.nextInt(9);
+                int randomCol = random.nextInt(9);
+
+                if (game.buyAndPlacePatch(patch.getPatchID(), randomRow, randomCol, randomRotation)) return;
+            }
+        }
+        game.pass();
     }
 }
