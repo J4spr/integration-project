ffb35d60dafbb1adf2137756c05be0124af79f1d
YariDeSchepper <yari.deschepper@student.kdg.be>
2026-03-16 21:33:30 +0100

    Fixed neutralTokenPosition in removePatch for circular PatchStack logic

--- src/main/java/be/kdg/programming/integrationproject/model/PatchStack.java
+++ src/main/java/be/kdg/programming/integrationproject/model/PatchStack.java
@@ -36,13 +36,14 @@ public class PatchStack {
      //Removes the patch with the given patchID from the list
      //Moves the neutral token to the position of the removed patch
      //Returns the removed patch
-    public Patch removePatch(int patchID) {
-        for (int i = 0; i < patches.size(); i++) {
-            if (patches.get(i).getPatchID() == patchID) {
-                neutralTokenPosition = i;
-                return patches.remove(i);
-            }
-        }
-        return null;
-    }
+     public Patch removePatch(int patchID) {
+         for (int i = 0; i < patches.size(); i++) {
+             if (patches.get(i).getPatchID() == patchID) {
+                 Patch removed = patches.remove(i);
+                 neutralTokenPosition = i % patches.size();
+                 return removed;
+             }
+         }
+         return null;
+     }
 }
