130a84f8ff985e6251bb66987cae97d1f1c10dfd
J4spr2 <jasper.verbruggen@student.kdg.be>
2026-03-17 17:48:21 +0100

    added logic to all of the classes so the DAO can return a class as datatype

--- src/main/java/be/kdg/programming/integrationproject/model/Patch.java
+++ src/main/java/be/kdg/programming/integrationproject/model/Patch.java
@@ -1,41 +1,29 @@
 package be.kdg.programming.integrationproject.model;
 
-import be.kdg.programming.integrationproject.model.Enums.PatchRotation;
-import be.kdg.programming.integrationproject.model.Enums.PatchShape;
-
 public class Patch {
-    private int patchID;
-    private PatchShape shape;
-    private PatchRotation rotation;
+    private int patchId;
     private int buttonCost;
     private int timeCost;
     private int buttonIncome;
 
-    public Patch() {
+    public Patch() {}
 
+    public Patch(int patchId, int buttonCost, int timeCost, int buttonIncome) {
+        this.patchId = patchId;
+        this.buttonCost = buttonCost;
+        this.timeCost = timeCost;
+        this.buttonIncome = buttonIncome;
     }
 
-    public int getPatchID() {
-        return this.patchID;
-    }
+    public int getPatchId() { return patchId; }
+    public void setPatchId(int patchId) { this.patchId = patchId; }
 
-    public PatchShape getShape() {
-        return this.shape;
-    }
+    public int getButtonCost() { return buttonCost; }
+    public void setButtonCost(int buttonCost) { this.buttonCost = buttonCost; }
 
-    public PatchRotation getRotation() {
-        return this.rotation;
-    }
+    public int getTimeCost() { return timeCost; }
+    public void setTimeCost(int timeCost) { this.timeCost = timeCost; }
 
-    public int getButtonCost() {
-        return this.buttonCost;
-    }
-
-    public int getTimeCost() {
-        return this.timeCost;
-    }
-
-    public int getButtonIncome() {
-        return this.buttonIncome;
-    }
+    public int getButtonIncome() { return buttonIncome; }
+    public void setButtonIncome(int buttonIncome) { this.buttonIncome = buttonIncome; }
 }
--- src/main/java/be/kdg/programming/integrationproject/model/Turn.java
+++ src/main/java/be/kdg/programming/integrationproject/model/Turn.java
@@ -1,4 +1,31 @@
 package be.kdg.programming.integrationproject.model;
 
+import java.sql.Time;
+
 public class Turn {
+    private int turnId;
+    private int gameId;
+    private Time turnStartTime;
+    private Time turnEndTime;
+
+    public Turn() {}
+
+    public Turn(int turnId, int gameId, Time turnStartTime, Time turnEndTime) {
+        this.turnId = turnId;
+        this.gameId = gameId;
+        this.turnStartTime = turnStartTime;
+        this.turnEndTime = turnEndTime;
+    }
+
+    public int getTurnId() { return turnId; }
+    public void setTurnId(int turnId) { this.turnId = turnId; }
+
+    public int getGameId() { return gameId; }
+    public void setGameId(int gameId) { this.gameId = gameId; }
+
+    public Time getTurnStartTime() { return turnStartTime; }
+    public void setTurnStartTime(Time turnStartTime) { this.turnStartTime = turnStartTime; }
+
+    public Time getTurnEndTime() { return turnEndTime; }
+    public void setTurnEndTime(Time turnEndTime) { this.turnEndTime = turnEndTime; }
 }
