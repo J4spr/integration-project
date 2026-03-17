db4b74e4549ef6d0f045773b659f2c65f55cfe03
J4spr2 <jasper.verbruggen@student.kdg.be>
2026-03-17 17:48:21 +0100

    added logic to all of the classes so the DAO can return a class as datatype

--- src/main/java/be/kdg/programming/integrationproject/model/Move.java
+++ src/main/java/be/kdg/programming/integrationproject/model/Move.java
@@ -1,24 +1,45 @@
 package be.kdg.programming.integrationproject.model;
 
-import be.kdg.programming.integrationproject.model.Enums.PatchRotation;
-
 import java.sql.Time;
 
 public class Move {
     private int moveId;
-    private Turn turnId;
-    private Patch patchId;
-    private Time startTime;
-    private Time endTime;
+
+    private int turnId;
+    private int patchId;
+
+    private Time moveStartTime;
+    private Time moveEndTime;
+
     private int specialPatchesCollected;
     private int spacesMoved;
     private int position;
-    private PatchRotation rotation;
-    private int ButtonsP1;
-    private int ButtonsP2;
+    private int rotationDegrees;
+    private int buttonsP1;
+    private int buttonsP2;
 
-    public Move(Time startTime, Time endTime){
+    public Move() {}
 
+    // Full constructor for the DAO to use when fetching from the DB
+    public Move(int moveId, int turnId, int patchId, Time moveStartTime, Time moveEndTime,
+                int specialPatchesCollected, int spacesMoved, int position,
+                int rotationDegrees, int buttonsP1, int buttonsP2) {
+        this.moveId = moveId;
+        this.turnId = turnId;
+        this.patchId = patchId;
+        this.moveStartTime = moveStartTime;
+        this.moveEndTime = moveEndTime;
+        this.specialPatchesCollected = specialPatchesCollected;
+        this.spacesMoved = spacesMoved;
+        this.position = position;
+        this.rotationDegrees = rotationDegrees;
+        this.buttonsP1 = buttonsP1;
+        this.buttonsP2 = buttonsP2;
     }
 
+    // Getters and Setters for all fields...
+    public int getMoveId() { return moveId; }
+    public void setMoveId(int moveId) { this.moveId = moveId; }
+
+    // ... repeat for other fields
 }
